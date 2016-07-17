3a476632918f654600a8f1cb4b97634ed26df3ce
Ingo Fischer <github@fischer-ka.de>
2016-07-17 22:05:25 +0200

    Added sleep() and isBusy() to example

--- examples/ReadSensorData/ReadSensorData.ino
+++ examples/ReadSensorData/ReadSensorData.ino
@@ -6,7 +6,7 @@ I2CSoilMoistureSensor sensor;
 void setup() {
   Wire.begin();
   Serial.begin(9600);
-  
+
   sensor.begin(); // reset sensor
   delay(1000); // give some time to boot up
   Serial.print("I2C Soil Moisture Sensor Address: ");
@@ -17,10 +17,12 @@ void setup() {
 }
 
 void loop() {
+  while (sensor.isBusy()) delay(50); // available since FW 2.3
   Serial.print("Soil Moisture Capacitance: ");
   Serial.print(sensor.getCapacitance()); //read capacitance register
   Serial.print(", Temperature: ");
   Serial.print(sensor.getTemperature()/(float)10); //temperature register
   Serial.print(", Light: ");
   Serial.println(sensor.getLight(true)); //request light measurement, wait and read light register
+  sensor.sleep(); // available since FW 2.3
 }
